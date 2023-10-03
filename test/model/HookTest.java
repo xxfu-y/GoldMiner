6e057f351074efb9498a931dc61008f5775894f1
zzl <1184047606@qq.com>
2023-10-03 16:49:49 +0800

    zzl_test

---
+++ test/model/HookTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HookTest {
+    private Hook hook;
+    private Miner miner;
+
+    @Before
+    public void init(){
+        miner = new Miner(new Location(0.4, 0.1));
+        hook = new Hook(new Location(0.4, 0.17), miner);
+    }
+    @After
+    public void clear(){
+        miner = null;
+        hook = null;
+    }
+
+    @Test
+    public void initTest(){
+        // 测试钩子与矿工的绑定
+        Assert.assertEquals(hook.getLocation().x, miner.getHook().getLocation().x, 1e-7);
+        Assert.assertEquals(hook.getLocation().y, miner.getHook().getLocation().y, 1e-7);
+    }
+
+}
+
